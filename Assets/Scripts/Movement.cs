37e6e5fd1dc96a6e01dc6e7128eaf807b5df8a0b
Redd <35120080+reddcaruana@users.noreply.github.com>
2026-02-20 15:07:19 +0100

    Basic Keyboard Controls

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
@@ -6,7 +7,10 @@ public class Movement : MonoBehaviour
     // String -> Data type
     public string playerName = "Ben Dover";
     public int lives = 3;
+    
     public float movementSpeed = 5.5f;
+    public float rotationSpeed = 180f;
+    
     public bool isAlive = true;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,6 +22,29 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // A basic condition to check
+        // for the W key
+        if (Keyboard.current.wKey.isPressed)
+        {
+            transform.Translate(Vector3.up * movementSpeed * Time.deltaTime);
+        }
+
+        if (Keyboard.current.sKey.isPressed)
+        {
+            transform.Translate(Vector3.down * movementSpeed * Time.deltaTime);
+        }
+
+        if (Keyboard.current.aKey.isPressed)
+        {
+            transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+        }
         
+        if (Keyboard.current.dKey.isPressed)
+        {
+            transform.Rotate(Vector3.back, rotationSpeed * Time.deltaTime);
+        }
+        
+        // Accesses the Transform component
+        // transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed);
     }
 }
